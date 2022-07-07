3d0de50c5bc5e284e3f74281fa141ce91af41655
hurrhnn <20sunrin022@sunrint.hs.kr>
2022-07-07 12:43:10 +0900

    bufep/debug: implemented initial debug c macros.

---
+++ include/bufep_debug.h
@@ -0,0 +1,50 @@
+#ifndef LIBBUFEP_BUFEP_DEBUG_H
+#define LIBBUFEP_BUFEP_DEBUG_H
+
+#include "bufep_config.h"
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+#define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+#else
+#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+#endif
+
+enum bufep_debug_colors {
+    RED = 0,
+    GRN,
+    YEL,
+    BLU,
+    MAG,
+    CYN,
+    WHT
+};
+
+#define STR_RST   "\x1B[0m"
+#define STR_RED   "\x1B[31m"
+#define STR_GRN   "\x1B[32m"
+#define STR_YEL   "\x1B[33m"
+#define STR_BLU   "\x1B[34m"
+#define STR_MAG   "\x1B[35m"
+#define STR_CYN   "\x1B[36m"
+#define STR_WHT   "\x1B[37m"
+
+#define BUFEP_DEBUG_COLOR(ENUM) \
+    (((ENUM) == RED) ? STR_RED :   \
+     ((ENUM) == GRN) ? STR_GRN :   \
+     ((ENUM) == YEL) ? STR_YEL :   \
+     ((ENUM) == BLU) ? STR_BLU :   \
+     ((ENUM) == MAG) ? STR_MAG :   \
+     ((ENUM) == CYN) ? STR_CYN :   \
+     ((ENUM) == WHT) ? STR_WHT : "" )
+
+#if BUFEP_ENABLE_DEBUG
+#define BUFEP_DEBUG(CLR, fmt, ...) \
+    printf("%s" fmt "%s", (BUFEP_DEBUG_COLOR(CLR)), ##__VA_ARGS__, STR_RST)
+#define BUFEP_DEBUG_MORE(CLR, fmt, ...) \
+    printf("%s [%s:%02d]: %s() - " fmt "%s", (BUFEP_DEBUG_COLOR(CLR)), __FILENAME__, __LINE__, __func__, ##__VA_ARGS__, STR_RST)
+#else
+#define BUFEP_DEBUG(CLR, fmt, ...) do {} while (0)
+#define BUFEP_DEBUG_MORE(CLR, fmt, ...) do {} while (0)
+#endif
+
+#endif //LIBBUFEP_BUFEP_DEBUG_H
--- src/bufep_debug.c
+++ src/bufep_debug.c
@@ -1,17 +1,2 @@
 #include "bufep_debug.h"
 
-int bufep_debug(const char * str, ...) {
-    return printf("%s", str);
-}
-
-int main() {
-    printf(RED "red\n"     RST);
-    printf(GRN "green\n"   RST);
-    printf(YEL "yellow\n"  RST);
-    printf(BLU "blue\n"    RST);
-    printf(MAG "magenta\n" RST);
-    printf(CYN "cyan\n"    RST);
-    printf(WHT "white\n"   RST);
-
-    return 0;
-}
