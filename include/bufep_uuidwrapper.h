233c2ea8af98005932f5c5cbe3a217f3c96702b7
hurrhnn <20sunrin022@sunrint.hs.kr>
2022-07-09 19:13:55 +0900

    bufep/config: correct the macro conditions.

--- include/bufep_uuidwrapper.h
+++ include/bufep_uuidwrapper.h
@@ -1,13 +1,13 @@
 #ifndef LIBBUFEP_BUFEP_UUIDWRAPPER_H
 #define LIBBUFEP_BUFEP_UUIDWRAPPER_H
 
-#if defined(HAVE_UUID_H)
-#include <uuid.h>
-#elif defined(HAVE_UUID_UUID_H)
-#include <uuid/uuid.h>
-#endif
+#include "bufep_config.h"
 
-#ifdef MS_WINDOWS
+#ifdef BUFEP_LINUX
+#include <uuid.h>
+#elif defined(BUFEP_MAC_OS_X)
+#include <uuid/uuid.h>
+#elif defined(MS_WINDOWS)
 #include <rpc.h>
 #endif
 
